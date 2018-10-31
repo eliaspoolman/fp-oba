fb17f5de0820350346c3f608542de03acb973027
Elias Poolman <eliaspoolman@gmail.com>
2018-10-31 13:11:53 +0100

    Eerste opzet en aantekeningen

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # fp-oba
+
+Dag 1 - Maandag 29 oktober 2018
+
+Dag 2 - Dinsdag 30 oktober 2018
+
+Dag 3 - Woensdag 31 oktober 2018
+
+Voorstel van stappenplan (via presentatie van Laurens):
+- Doe een succesvolle request waaruit je JSON haalt
+- Krijg een idee van de informatie die je uit de API kan halen.
+- Verzin onderzoeksvragen en deelvragen
+- Bedenk welke variabelen je nodig hebt
+- Haal data uit de API en sla die op in jouw eigen "datastore"
+- Doorzoek de data op patronen
+- Visualiseer de patronen met D3
---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const api = require('./oba-api.js')
+const chalk = require('chalk');
+const express = require('express')
+const app = express()
+const port = 3000
+
+const obaApi = new api({
+  url: 'https://zoeken.oba.nl/api/v1/',
+  key: process.env.PUBLIC
+})
+
+// Search for method, params and than optional where you wanna find something
+// returns first 20 items
+// obaApi.get(endpoint, params, filterKey)
+// possible endpoints: search (needs 'q' parameter) | details (needs a 'frabl' parameter) | availability (needs a 'frabl' parameter) | holdings/root | index/x (where x = facet type (like 'book' ))
+// possible parameters: q, librarian, refine, sort etc. check oba api documentation for all
+// possible filterKey: any higher order key in response object, like title returns only title objects instead of full data object
+obaApi.get('search', {
+  q: 'harry potter',
+  librarian: true,
+  refine: true
+}, 'title').then(response => {
+
+  // response ends up here
+  console.log(response)
+
+  // Make server with the response on the port
+  app.get('/', (req, res) => res.json(response))
+  app.listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
+})
